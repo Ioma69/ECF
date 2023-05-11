31f44424aadb59582b3162bb105817871d00709a
Sariel RAQUIN <sarielr@hotmail.fr>
2023-05-12 01:25:52 +0200

    fix

--- migrations/Version20230508205732.php
+++ migrations/Version20230508205732.php
@@ -20,7 +20,7 @@ final class Version20230508205732 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE debug');
+        
         
     }
 
