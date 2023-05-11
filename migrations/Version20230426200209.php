05648e7a1a90f62fe21127bf0b771f9c465532f1
Sariel RAQUIN <sarielr@hotmail.fr>
2023-05-12 01:20:34 +0200

    fix

--- migrations/Version20230426200209.php
+++ migrations/Version20230426200209.php
@@ -20,7 +20,7 @@ final class Version20230426200209 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE dishes DROP admin_id, DROP categories_id');
+
     }
 
     public function down(Schema $schema): void
