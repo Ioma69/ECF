9f5a5a9470a1747a0492e6597e1eb3135324f8d6
Sariel RAQUIN <sarielr@hotmail.fr>
2023-05-12 01:44:42 +0200

    fix

--- migrations/Version20230511200936.php
+++ migrations/Version20230511200936.php
@@ -24,8 +24,7 @@ final class Version20230511200936 extends AbstractMigration
         
     
        
-        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C84955A76ED395');
-        $this->addSql('DROP INDEX IDX_42C84955A76ED395 ON reservation');
+        
         $this->addSql('DROP INDEX IDX_42C8495570BEE6D ON reservation');
         $this->addSql('ALTER TABLE reservation DROP user_id, DROP visitor_id');
         $this->addSql('ALTER TABLE schedule DROP FOREIGN KEY FK_5A3811FB39118BDC');
